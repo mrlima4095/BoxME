6b1659592b07f56e738be0b181da49c0adb0fd09
Lima <felipebr4095@gmail.com>
2025-05-11 22:23:49 -0300

    update install.sh

--- install.sh
+++ install.sh
@@ -3,9 +3,15 @@
 set BOXME_REPO=raw.githubusercontent.com/mrlima4095/main/BoxME/src
 
 install nano
+tick Downloading BoxME...
 proxy BOXME_REPO/boxme
 install boxme 
 proxy BOXME_REPO/boxme-suite
 install .boxme-suite
 proxy BOXME_REPO/boxme-loader
 install .boxme-loader
+tick
+
+
+log add BoxME have been installed!
+echo [ BoxME ] Installation Complete!
