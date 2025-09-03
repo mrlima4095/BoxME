e7a927d2d85d61a9df779b445e52058a38445ba6
Mr Lima <felipebr4095@gmail.com>
2025-09-03 13:46:26 -0300

    install.sh - prefer Lua installation

--- install.sh
+++ install.sh
@@ -5,7 +5,7 @@ set BOXME_REPO=raw.githubusercontent.com/mrlima4095/BoxME/refs/heads/main/src/
 install nano
 tick Downloading BoxME...
 
-execute proxy $BOXME_REPO/boxme.lua; install boxme 
+execute proxy $BOXME_REPO/boxme.lua; install boxme.lua 
 
 get nano
 tick
