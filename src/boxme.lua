5e35c62bb2937e67efc7b0a37f720f0f41482623
Lima <felipebr4095@gmail.com>
2025-09-08 11:51:28 -0300

    update boxme.lua

--- src/boxme.lua
+++ src/boxme.lua
@@ -26,34 +26,3 @@ update=execute clone raw.githubusercontent/mrlima4095/BoxME/main/install.sh;
 
 ]]
 
-local g = require("graphics")
-
-
-local function main()
-    local STATE = os.getenv("VM2ME")
-
-    if STATE == nil then
-        g.SetTicker("Loading...")
-        
-        os.execute("execute set BOXME_VERSION=1.0; ")
-
-    elseif STATE == "MAIN_MENU" then
-        local menu = {
-            title = "BoxME Menu",
-            itens = {
-                ["New Machine"] = "execute set VM2ME=NEW",
-                ["Start a Machine"] = "execute set VM2ME=START",
-                ["Settings"] = "execute set VM2ME=SETTINGS",
-                ["Documentation"] = "view <title>Documentation</title>",
-                ["About"] = "execute set VM2ME=MENU; boxme; set OLD_TITLE=$TITLE; title BoxME $BOXME_VERSION; warn BoxME $BOXME_VERSION\n(C) 2025 - Mr. Lima\n\nBoxME runs Virtual Machines into J2ME Environment!; title $OLD_TITLE; unset $OLD_TITLE;"
-            }
-        }
-
-        g.BuildList(menu)
-    end
-
-
-end
-
-
-main()
