676c494cf880523fa043bf3513a58319bda0e743
Mr Lima <felipebr4095@gmail.com>
2025-09-12 20:53:44 -0300

    BoxME - Lua Main Menu

--- src/boxme.lua
+++ src/boxme.lua
@@ -11,10 +11,9 @@ api.match=minimum
 process.name=boxhost2
 process.exit=execute unset VM2ME; log add BoxME have been killed; echo [ BoxME ] Host service killed; true
 
-config=lua boxme.lua
 command=boxme
 
-boxme=execute set VM2ME=MAIN_MENU;
+boxme=execute ;
 
 shell.name=boxme
 shell.args=new,start,help,update
@@ -26,3 +25,43 @@ update=execute clone raw.githubusercontent/mrlima4095/BoxME/main/install.sh;
 
 ]]
 
+local host = os.getpid("boxhost2")
+
+graphics = {
+    display = function (screen) end,
+    Alert = function (config) end,
+    BuildScreen = function (config) end,
+    BuildList = function (config) end,
+    BuildQuest = function (config) end,
+    BuildEdit = function (config) end,
+    SetTitle = function (title) end,
+    WindowTitle = function (title) end,
+    SetTicker = function (text) end
+}
+
+local function menu()
+    graphics.display(graphics.BuildList({
+        title = "BoxME",
+        back = { root = os.exit },
+        button = {
+            label = "Select",
+            root = function (option)
+                
+            end
+        },
+
+        fields = { "New Machine", "Start a Machine", "Settings", "About" }
+    }))
+end
+
+local function main()
+    if host == nil then error("BoxME service isn't running!") end
+
+    local state = os.getproc(host, "state")
+
+    if state == nil then
+        menu()
+    end
+end
+
+main()
