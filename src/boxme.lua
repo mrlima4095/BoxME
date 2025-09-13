70470d6760b68afdf9ac549bc0a7a20a5033519c
Mr Lima <felipebr4095@gmail.com>
2025-09-13 08:19:02 -0300

    BoxME - OpenTTY Package

--- src/boxme.lua
+++ src/boxme.lua
@@ -5,23 +5,14 @@ version=1.0
 description=Virtual Machines
 
 api.version=1.16
-api.error=execute log add error BoxME requires OpenTTY API 1.14; echo [ BoxME ] Loading failed!; echo [ BoxME ] See logs to more info.;
+api.error=execute log add error BoxME requires OpenTTY API 1.16; echo [ BoxME ] Loading failed!; echo [ BoxME ] See logs to more info.;
 api.match=minimum
 
 process.name=boxhost2
-process.exit=execute unset VM2ME; log add BoxME have been killed; echo [ BoxME ] Host service killed; true
+process.exit=execute log add BoxME have been killed; echo [ BoxME ] Host service killed; true
 
 command=boxme
-
-boxme=execute ;
-
-shell.name=boxme
-shell.args=new,start,help,update
-
-new=execute boxme-new new
-start=execute boxme-loader init
-help=execute install nano; get .boxme-README; html; get nano;
-update=execute clone raw.githubusercontent/mrlima4095/BoxME/main/install.sh;
+boxme=lua boxme
 
 ]]
 
